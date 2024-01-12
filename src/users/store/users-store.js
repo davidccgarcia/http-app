095992060e83c03710d39c5f353af237b704a88f
David García <ccristhiangarcia@gmail.com>
2024-01-12 07:48:07 -0500

    Actualizar nuestro store

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -6,7 +6,12 @@ const state = {
 }
 
 const loadNextPage = async() => {
-    await loadUsersByPage( state.currentPage + 1 );
+    const users = await loadUsersByPage( state.currentPage + 1 );
+
+    if ( users.length === 0) return;
+
+    state.currentPage += 1;
+    state.users = users;
 }
 
 const loadPreviousPage = async() => {
@@ -27,6 +32,6 @@ export default {
     onUserChanged,
     reloadPage,
 
-    getUser: () => [...state.users],
+    getUsers: () => [...state.users],
     getCurrentPage: () => state.currentPage,
 }
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -6,6 +6,7 @@ import usersStore from "./store/users-store";
  */
 export const UsersApp = async( element ) => {
     element.innerHTML = 'Loading...';
-
     await usersStore.loadNextPage();
+
+    console.log( usersStore.getUsers() );
 }
