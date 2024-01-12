37aff38972b20605733778ed317fd83b44c77d7b
David García <ccristhiangarcia@gmail.com>
2024-01-12 07:43:49 -0500

    Mappers

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -1,12 +1,17 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
+
 /**
  * 
  * @param {Number} page 
- * @returns
+ * @returns {Promise<User[]>}
  */
 export const loadUsersByPage = async( page = 1 ) => {
     const url = `${ import.meta.env.VITE_PAGE_URL }users?_page=${ page }`;
-    const res = await fetch( url );
-    const data = await res.json();
+    const response = await fetch( url );
+    const resolve = await response.json();
 
-    console.log(data);
+    const users = resolve.data.map( localhostUserToModel );
+
+    return users;
 }
