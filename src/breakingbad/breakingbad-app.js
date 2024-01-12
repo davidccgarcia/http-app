9859db686130fbe20500a98734fe24c693708e4b
David García <ccristhiangarcia@gmail.com>
2024-01-12 05:48:25 -0500

    Breakingbad component

---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = ( element ) => {
+    document.querySelector('#app-title').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+}
