043564def83145c7341c952e6e3e56b4cb14be4a
David García <ccristhiangarcia@gmail.com>
2024-01-20 20:13:07 -0500

    Caso de uso - Crear usuario

---
+++ src/users/use-cases/save-user.js
@@ -0,0 +1,33 @@
+import { User } from '../models/user';
+
+/**
+ * 
+ * @param {Like<User>} userLike 
+ */
+export const saveUSer = async( userLike ) => {
+    const user = new User( userLike );
+
+    // TODO: aquí falta un mapper
+    if ( user.id ) {
+        throw 'Not implemented.';
+    }
+
+    const updatedUser = await createUser( user );
+}
+
+/**
+ * @param {Like<User>} user
+ */
+const createUser = async( user ) => {
+    const url = `${ import.meta.env.VITE_BASE_URL }/users`;
+    const res = await fetch( url, {
+        method: 'POST', body: JSON.stringify( user ), 
+        header: {
+            'COntent-Type': 'application/json'
+        }
+    });
+
+    const newUser = await res.json();
+    console.log({ newUser });
+    return newUser;
+}
